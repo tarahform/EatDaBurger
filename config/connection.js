e870b0eca10e2d733efee5affe85469ea308b181
tarahform <ptarahform@gmail.com>
2018-05-29 16:35:49 -0500

    finished

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+var mysql = require("mysql");
+
+// setting connection to mysql
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "root",
+  database: "burgers_db"
+});
+
+// .connect method connects to mysql
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// exporting so that we have access to mysql outside of this file
+module.exports = connection;  
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burger_db;
+
+INSERT INTO burgers (burger_name) VALUES ('Texas Burger');
+INSERT INTO burgers (burger_name) VALUES ('Stupid Chicago Burger');
+INSERT INTO burgers (burger_name) VALUES ('Vegan Burger');
