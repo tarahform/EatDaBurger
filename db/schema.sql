1fda0f06ef5b934ae4afda23e187951f4ee1f8e3
tarahform <ptarahform@gmail.com>
2018-05-29 17:52:44 -0500

    style added

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,10 @@
-DROP DATABASE IF EXISTS burgers_db;
-CREATE DATABASE burgers_db;
-USE burgers_db;
+USE nd7zp542ak0cmaft;
 
 CREATE TABLE burgers
 (
 id int NOT NULL AUTO_INCREMENT,
 burger_name VARCHAR(255) NOT NULL,
 devoured BOOLEAN NOT NULL DEFAULT 0,
+createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
 PRIMARY KEY (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,4 @@
-USE burger_db;
-
+USE nd7zp542ak0cmaft;
 INSERT INTO burgers (burger_name) VALUES ('Texas Burger');
 INSERT INTO burgers (burger_name) VALUES ('Stupid Chicago Burger');
 INSERT INTO burgers (burger_name) VALUES ('Vegan Burger');
